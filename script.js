e6a53f366eeb477b5d18b4fa35a56aacffc0922f
inomhus <ted.ohman@hyperisland.se>
2024-11-07 10:40:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ const FileManager = {
 const TreeView = {
     async initialize() {
         try {
-            const response = await fetch('/structure');
+            const response = await fetch('/structure.json');
             const structure = await response.json();
             this.buildTree(structure, UI.elements.fileTree);
         } catch (error) {
